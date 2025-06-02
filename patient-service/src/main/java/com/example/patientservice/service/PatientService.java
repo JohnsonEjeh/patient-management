532e6f2723476c1b9870cdf4c6a7491301a14034
JohnsonEjeh <ejehsax@gmail.com>
2025-06-01 22:01:22 -0400

    kafka patient service producer working.

--- patient-service/src/main/java/com/example/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/example/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.example.patientservice.dto.PatientResponseDTO;
 import com.example.patientservice.exception.EmailAlreadyExistsException;
 import com.example.patientservice.exception.PatientNotFoundException;
 import com.example.patientservice.grpc.BillingServiceGrpcClient;
+import com.example.patientservice.kafka.KafkaProducer;
 import com.example.patientservice.mapper.PatientMapper;
 import com.example.patientservice.model.Patient;
 import com.example.patientservice.repository.PatientRepository;
@@ -20,6 +21,7 @@ import java.util.UUID;
 public class PatientService {
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
     public List<PatientResponseDTO> getAllPatients() {
         List<Patient> patients = patientRepository.findAll();
@@ -34,6 +36,7 @@ public class PatientService {
         Patient newPatient = patientRepository.save(PatientMapper.toPatient(patientRequestDTO));
 
         billingServiceGrpcClient.createBilling(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
+        kafkaProducer.sendEvent(newPatient);
 
         return PatientMapper.toDTO(newPatient);
     }
